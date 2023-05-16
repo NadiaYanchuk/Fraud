0885f6b993e3dc16c203b9c4e24c3059bf5b2c18
Nadejda Ianciuc <nadiaianciuk@gmail.com>
2023-05-16 19:43:20 +0300

    Template

---
+++ resources/views/frontend.blade.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fraud</title>
+    <link href="/css/null.css" rel="stylesheet" type="text/css">
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+  <header>
+    <div class="header-background">
+        <nav>
+            <ul>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Learn</a></li>
+                <li><a href="#">Company</a></li>
+            </ul>
+        </nav>
+        <h1>One platform for fraud, compliance, and payments</h1>
+        <p>Combine risk, compliance, and payment protection to increase customer trust and loyalty - all from one powerful API.</p>
+        <div class="header-button">
+            <div class="icon"></div>
+            <button>Contact us</button>
+        </div>
+    </div>
+  </header>
+
+  <section>
+    <div class="part1">
+      <div class="gray-block">
+        <h2>Trusted by</h2>
+      </div>
+    </div>
+    
+    <div class="part2">
+      <div class="text-columns">
+        <div class="column">
+          <p class="column1">By the numbers</p>
+        </div>
+        <div class="column">
+          <h1>Increase your revenue with an advanced fraud pipeline</h1>
+        </div>
+      </div>
+      <div class="blocks">
+        <div class="block1">
+          <div class="icon"></div>
+          <h3>7x</h3>
+          <p>Fraud prevention</p>
+        </div>
+        <div class="block1">
+          <div class="icon"></div>
+          <h3>2x</h3>
+          <p>Less false positives</p>
+        </div>
+        <div class="block1">
+          <div class="icon"></div>
+          <h3>98%</h3>
+          <p>Authorization rates</p>
+        </div>
+        <div class="block1">
+          <div class="icon"></div>
+          <h3>10x</h3>
+          <p>Higher order value</p>
+        </div>
+      </div>
+    </div>
+    
+    <hr>
+
+    <div class="part3">
+      <div class="text-columns">
+        <div class="column">
+          <p class="column1">All-in-one platform</p>
+        </div>
+        <div class="column">
+          <h1>Hire the world’s best fraud fighters with one API</h1>
+          <p>Sardine’s comprehensive platform is designed to help fintech and crypto companies build and grow with confidence. From onboarding fraud and KYC/AML to account funding and payments fraud, our world-class team is at your service.</p>
+        </div>
+      </div>
+      <div class="image-columns">
+        <div class="column">
+          <div class="image"></div>
+          <h3>Risk</h3>
+          <p>One API to prevent fraud, and manage compliance across your customer lifecycle.</p>
+      </div>
+      <div class="column">
+        <div class="image"></div>
+        <h3>Payments</h3>
+        <p>Move money instantly, and let Sardine handle the fraud, regulatory and legal risk.</p>
+      </div>
+      <div class="column">
+        <div class="image"></div>
+        <h3>Insights</h3>
+        <p>Make more informed risk decisions at the intersection of traditional finance and crypto.</p>
+      </div>
+    </div>
+  </div>
+  
+  <hr>
+  
+  <div class="part4">
+    <div class="text-columns">
+      <div class="column">
+        <p class="column1">Always-on protection</p>
+      </div>
+      <div class="column">
+        <h1>We detect fraudsters by their signature moves</h1>
+        <p>Our proprietary Device Intelligence and Behavior Biometrics technology analyzes thousands of signals in real-time, to stop more scams, before they wreak havoc for you and your customers.</p>
+        <div class="button">
+            <div class="icon"></div>
+            <button>Contact us</button>
+        </div>
+      </div>
+    </div>
+    <div class="blocks">
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Fraud rings</h3>
+        <p>Mobile device farms, bot attacks, SIM-swaps, copy & paste malware, phishing websites, fake mobile apps.</p>
+      </div>
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Social engineering</h3>
+        <p>Investment advisor, romance scams, fake support, remote desktop ATO, refund, giveaway scams.</p>
+      </div>
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Identity theft</h3>
+        <p>Stolen and synthetic identity fraud, fake and pre-recorded document verification.</p>
+      </div>
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Financial crime</h3>
+        <p>Stolen bank and card credentials. Card chargeback abuse, bank ACH insufficient funds (NSF) returns.</p>
+      </div>
+    </div>
+  </div>
+  
+  <hr>
+  
+  <div class="part5">
+    <div class="quotation-block">
+      <div class="image"></div>
+      <div class="icon">
+        <img src="/images/quote.svg">
+      </div>
+      <div class="text">
+        <p>We’re excited to offer MetaMask users instant ACH funding with Sardine. Instant ACH allows higher limits than cards and has a higher authorization rate than cards. So it is game-changing for our users to instantly move dollars in their bank to crypto in their MetaMask wallet.</p>
+        <div class="line"></div>
+        <div class="author">
+          <p>Lorenzo Santos</p>
+          <p>Product Manager, MetaMask</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="part6">
+  <div class="text-columns">
+      <div class="column">
+        <p>Instant settlement</p>
+      </div>
+      <div class="column">
+        <h1>Don’t make your customers wait 2-3 days for their money</h1>
+        <p>Instant fund access with a higher purchase allowance - makes for happier, repeat customers. Grow your revenue faster and let Sardine handle the fraud and chargeback risk.</p>
+      </div>
+    </div>
+
+    <div class="image-columns">
+      <div class="column">
+        <div class="image"></div>
+        <h3>Digital Wallets</h3>
+        <p>Instant account funding via card or bank</p>
+      </div>
+      <div class="column">
+        <div class="image"></div>
+        <h3>Crypto On-Ramp</h3>
+        <p>Instant delivery of 60+ tokens on 20+ chains</p>
+      </div>
+      <div class="column">
+        <div class="image"></div>
+        <h3>NFT Checkout</h3>
+        <p>The easiest and fastest way to buy an NFT</p>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="part7">
+  <div class="text-columns">
+      <div class="column">
+        <p>Why Sardine</p>
+      </div>
+      <div class="column">
+        <h1>We live in an era of faster money movement</h1>
+        <p>But Authorized Push Payments (APP) and the irreversible nature of crypto and digital asset transactions have attracted more and more sophisticated fraudsters.</p>
+        <p>Sardine provides both the technology and an unparalleled level of expertise that’s required to make payments safer, faster and ultimately smarter.”</p>
+    </div>
+    </div>
+    <div class="blocks">
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Stop fraudsters by their intrinsic behavior</h3>
+        <p>Our AI-powered Device Intelligence and Behavior Biometrics detects the latest social engineering scams and most advanced fraud rings.</p>
+      </div>
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Always-on fraud protection and compliance in one API</h3>
+        <p>90% of payment fraud comes from fully verified IDs. That’s why we combine KYC/AML with fraud prevention in a single platform.</p>
+      </div>
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Instant settlement for digital wallets</h3>
+        <p>Give customers instant access to their funds while unlocking higher authorization rates and higher purchase limits for crypto and NFTs.</p>
+      </div>
+      <div class="block">
+        <div class="icon"></div>
+        <h3>Bridging the visibility gap between TradFi and crypto</h3>
+        <p>We’re building the world’s largest data consortium combining off-chain and blockchain data to fight bank and card fraud.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="part8">
+    <div class="blue-block">
+      <h1>Announcing our Series-B led by Andreessen Horowitz Growth Fund</h1>
+      <p>Sardine recently raised $51.5M to accelerate product development and international expansion. Read our blog post by Soups Ranjan, CEO.</p>
+      <div class="button">
+        <div class="icon"></div>
+        <button>Learn more</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="part9"></div>
+  
+
+</section>
+
+
+    <!--
+
+    <div class="blue-block">
+        <h1>Announcing our Series-B led by Andreessen Horowitz Growth Fund</h1>
+        <p>Sardine recently raised $51.5M to accelerate product development and international expansion. Read our blog post by Soups Ranjan, CEO.</p>
+        <div class="button">
+            <div class="icon"></div>
+            <button>Learn more</button>
+        </div>
+    </div>
+
+    <div class="content-block">
+        <div class="text-column">
+            <p class="small-text">Get started with Sardine</p>
+            <h2>Safer, Faster Payments</h2>
+            <p>Your description goes here.</p>
+            <button>Contact Sales</button>
+        </div>
+        <div class="image-column">
+            
+        </div>
+    </div>
+  </section>
+
+  <footer class="footer">
+  <div class="column">
+    <h3>© Sardine. All right reserved.</h3>
+  </div>
+  <div class="column">
+    <h3>Platform</h3>
+    <ul>
+      <li>Fraud</li>
+      <li>Compliance</li>
+      <li>Crypto On-Ramp</li>
+      <li>NFT Checkout</li>
+      <li>Entity Risk</li>
+    </ul>
+  </div>
+  <div class="column">
+    <h3>Solutions for</h3>
+    <ul>
+      <li>Fintech</li>
+      <li>TradFi</li>
+      <li>Crypto</li>
+      <li>NFT</li>
+    </ul>
+  </div>
+  <div class="column">
+    <h3>Learn</h3>
+    <ul>
+      <li>Documentation</li>
+      <li>Blog</li>
+      <li>Events</li>
+    </ul>
+  </div>
+  <div class="column">
+    <h3>Company</h3>
+    <ul>
+      <li>About</li>
+      <li>Careers</li>
+    </ul>
+  </div>
+  <div class="bottom-row">
+    <ul>
+      <li>Privacy Policy</li>
+      <li>GDPR</li>
+      <li>Terms of Service</li>
+    </ul>
+  </div>
+</footer>  -->
+
+</body>
+
+</html>
